de89500fec1b52315bf16a9762d2a4d84084afeb
TriS <tris.1027@gmail.com>
2017-06-08 19:13:19 +1000

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  public activeTab = 'signIn';
+
+  public formModelSignup: FormGroup;
+
+  public formModelLogin: FormGroup;
+
+  public submit(values: any): void {
+    console.log(values);
+  }
+
+  public constructor(
+    private _fb: FormBuilder
+  ) { }
+
+  public ngOnInit(): void {
+    this.formModelSignup = this._fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4)]]
+    });
+
+    this.formModelLogin = this._fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  public setActive(tabName: string): void {
+    this.activeTab = tabName;
+  }
+
 }
